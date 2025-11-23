d8fcf6e042997fb3793a599b75665819e6858d42
livcia <ankiewicz_oliwia@wp.pl>
2025-11-23 12:27:36 +0100

    create scripts directory

--- scripts/startup-script.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y telnet
-sudo apt install -y nginx
-sudo systemctl enable nginx
-sudo chmod -R 755 /var/www/html
-HOSTNAME=$(hostname)
-sudo echo "<!DOCTYPE html> <html> <body style='background-color:rgb(275, 200, 200);'> <h1>Super, udalo nam się stworzyc projekt! </h1> <p><strong>VM Hostname:</strong> $HOSTNAME</p> <p><strong>VM IP Address:</strong> $(hostname -I)</p> <p><strong>Application Version:</strong> V1</p> <p>Pozdrawiamy, Zespol projektu 6, grupa 2 :)</p> </body></html>" | sudo tee /var/www/html/index.html
